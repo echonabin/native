bee848b534b0d8b543be10a84dbf1cd5eca1ffd5
echonabin <echonabin@gmail.com>
2020-07-12 03:01:33 +0545

    Button Added!!

--- src/screens/ButtonScreen.js
+++ src/screens/ButtonScreen.js
@@ -4,15 +4,15 @@ import { View, Text, StyleSheet, Button } from "react-native";
 const ButtonScreen = () => {
   return (
     <View>
+      <Button title='click me!' />
       <Text>Hello from button page!!</Text>
-      <Text>This shouldn't be added to git hub!!</Text>
-      <Text style={style.buttonStyle}>Text with design!!</Text>
+      <Text style={style.textStyle}>Text with design!!</Text>
     </View>
   );
 };
 
 const style = StyleSheet.create({
-  buttonStyle: {
+  textStyle: {
     fontSize: 40,
     fontWeight: "bold",
   },
