db032d723504f292558c28ecfa712479db4bc4cd
echonabin <echonabin@gmail.com>
2020-07-13 01:38:02 +0545

    SquareScreen with switch and ternitery expression added !!

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,21 +7,41 @@ const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+      default:
+        return;
+    }
+  };
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + COLOR_INCRIEMENT)}
-        onDecrease={() => setRed(red - COLOR_INCRIEMENT)}
+        onIncrease={() => setColor("red", COLOR_INCRIEMENT)}
+        onDecrease={() => setColor("red", -1 * COLOR_INCRIEMENT)}
         color='Red'
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + COLOR_INCRIEMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCRIEMENT)}
+        onIncrease={() => setColor("green", COLOR_INCRIEMENT)}
+        onDecrease={() => setColor("green", -1 * COLOR_INCRIEMENT)}
         color='Green'
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + COLOR_INCRIEMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCRIEMENT)}
+        onIncrease={() => setColor("blue", COLOR_INCRIEMENT)}
+        onDecrease={() => setColor("blue", -1 * COLOR_INCRIEMENT)}
         color='Blue'
       />
       <View
