3e42d95cce9b503ae699d906d7ca6bc6df9370ad
echonabin <echonabin@gmail.com>
2020-07-12 17:28:12 +0545

    touchopacity removed for now and button added!!

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,17 +2,14 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 import ComponentsScreen from "./ComponentsScreen";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text>Hi There!!</Text>
       <Button
         title='Components'
-        onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigation.navigate("Components")}
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate("Lists")}>
-        <Text>Lists</Text>
-      </TouchableOpacity>
+      <Button title='Lists' onPress={() => navigation.navigate("Lists")} />
     </View>
   );
 };
